e1ac3eb1e631f1d40a49f6444d7ce1d424233733
Cafnanc <cafnanc@gmail.com>
2024-05-28 20:54:57 +0530

    UI of chat and listing page

--- projects/frontend-chat-library/src/lib/frontend-chat-library.module.ts
+++ projects/frontend-chat-library/src/lib/frontend-chat-library.module.ts
@@ -2,21 +2,31 @@ import { NgModule } from '@angular/core';
 import { FrontendChatLibraryComponent } from './frontend-chat-library.component';
 import { ChatViewComponent } from './chat-view/chat-view.component';
 import { MessageListingComponent } from './message-listing/message-listing.component';
-
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
     FrontendChatLibraryComponent,
-    ChatViewComponent,
     MessageListingComponent,
     ChatViewComponent
   ],
   imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule
   ],
   exports: [
     FrontendChatLibraryComponent,
-    ChatViewComponent
+    ChatViewComponent,
+    MessageListingComponent
   ]
 })
 export class FrontendChatLibraryModule { }
--- projects/frontend-chat-library/src/lib/message-listing/message-listing.component.ts
+++ projects/frontend-chat-library/src/lib/message-listing/message-listing.component.ts
@@ -6,7 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-listing.component.css']
 })
 export class MessageListingComponent implements OnInit {
-
+  messages = [
+    {
+      image: 'https://via.placeholder.com/150',
+      name: 'John Doe',
+      lastMessage: 'Hey, how are you?',
+      time: new Date()
+    },
+    {
+      image: 'https://via.placeholder.com/150',
+      name: 'Jane Smith',
+      lastMessage: 'Are we meeting tomorrow?',
+      time: new Date(Date.now() - 600000)
+    },
+    {
+      image: 'https://via.placeholder.com/150',
+      name: 'Bob Johnson',
+      lastMessage: 'Call me when you are free.',
+      time: new Date(Date.now() - 1200000)
+    },
+    {
+      image: 'https://via.placeholder.com/150',
+      name: 'David',
+      lastMessage: 'Hey, how are you?',
+      time: new Date()
+    },
+    {
+      image: 'https://via.placeholder.com/150',
+      name: 'Jack Sparrow',
+      lastMessage: 'Are we meeting tomorrow?',
+      time: new Date(Date.now() - 600000)
+    },
+    {
+      image: 'https://via.placeholder.com/150',
+      name: 'Joseph',
+      lastMessage: 'Hey man!!.',
+      time: new Date(Date.now() - 1200000)
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
